bbd6a070ce7e13228a7122a33fda9973969e38fe
danyd3 <89928594+danyd3@users.noreply.github.com>
2021-10-05 17:16:45 -0500

    Add files via upload

---
+++ holaenpython.py
@@ -0,0 +1,9 @@
+#Hola mundo en python
+#Title                    :Hola mundo en Python
+#Description              :Hola mundo en Python desde Vim
+#Author                   :Daniel Alberto De Jesus (danyd3)
+#Date                     :20211005
+#Version                  :1
+#Usage                    :c:\python /path/holaenpython.py
+
+print ("Hola mundo en Python con Vim")
